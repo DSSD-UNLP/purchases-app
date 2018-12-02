53e7ae5f6616f6f509705d2af7d82a65496a9e1a
Julian Pasquale <julian.pasquale@snappler.com>
2018-12-02 12:24:41 -0300

    Working caching cookies

--- app/services/bonita_manager.rb
+++ app/services/bonita_manager.rb
@@ -1,8 +1,10 @@
 class BonitaManager
   require 'uri'
   require 'net/http'
+
   BONITA_USERNAME = 'walter.bates'.freeze
   BONITA_PASSWORD = 'bpm'.freeze
+  BONITA_HOST_URL = ENV['BONITA_HOST_URL']
 
   START_CASE_PARAMETERS = {
     'processDefinitionId': '4751000970449300331',
@@ -18,59 +20,60 @@ class BonitaManager
     ]
   }.freeze
 
-  attr_reader :connection, :bonita_token
+  attr_reader :connection
 
   def initialize
-    # session[:bonita_manager] = self
-    @connection = Faraday.new(url: ENV['BONITA_HOST_URL']) do |conn|
+    @connection = Faraday.new(url: BONITA_HOST_URL) do |conn|
       conn.response :logger
       conn.adapter Faraday.default_adapter
     end
-    set_bonita_token
-    start_case
+
+    @cookies = Rails.cache.fetch("#{BONITA_HOST_URL}/cookies") do
+      initialize_cookies(HTTP::CookieJar.new)
+    end
+    @cookies = initialize_cookies(HTTP::CookieJar.new) if @cookies.any?(&:expired?)
+  end
+
+  def start_case
+    response = connection.post('/bonita/API/bpm/case') do |request|
+      request.headers['X-Bonita-API-Token'] = bonita_token
+      request.headers['Content-Type']       = 'application/json'
+      request.headers['Cookie']             = parse_cookies
+      request.headers['cache-control']      = 'no-cache'
+      request.body = START_CASE_PARAMETERS.to_json
+    end
   end
 
   private
 
-  def set_bonita_token
-    response = connection.post do |request|
+  def login_service
+    connection.post do |request|
       request.url('/bonita/loginservice')
       request.params['username'] = BONITA_USERNAME
       request.params['password'] = BONITA_PASSWORD
       request.params['redirect'] = false
     end
-
-    @bonita_token = parse_bonita_token(response.headers.to_hash)
   end
 
-  def parse_bonita_token(headers)
-    @cookies      = headers['set-cookie']
-    bonita_token  = @cookies.split(',').find do |elem|
+  def initialize_cookies(jar)
+    @response_cookies = login_service.headers.to_hash['set-cookie']
+
+    @response_cookies.split(',').each do |value|
+      jar.parse(value, BONITA_HOST_URL)
+    end
+
+    jar.cookies(BONITA_HOST_URL)
+  end
+
+  def bonita_token
+    bonita_token = @response_cookies.split(',').find do |elem|
       elem.downcase.include? 'bonita-api-token'
     end
 
-    jsession = @cookies.split(',').find do |elem|
-      elem.downcase.include? 'jsession'
-    end
-
-    @jsession = jsession.split(';').first.strip.split('=').second
     bonita_token.split(';').first.strip.split('=').second
   end
 
-  def start_case
-    response = connection.post('/bonita/API/bpm/case') do |request|
-      request.headers['X-Bonita-API-Token'] = @bonita_token
-      request.headers['Content-Type']       = 'application/json'
-      request.headers['Cookie']             = get_cookies
-      request.headers['cache-control']      = 'no-cache'
-      request.body = START_CASE_PARAMETERS.to_json
-    end
-    binding.pry
-  end
-
-  def get_cookies
-    'bonita.tenant=1; path=/bonita; domain=localhost;' \
-    "X-Bonita-API-Token=#{@bonita_token}; path=/; domain=localhost;" \
-    "JSESSIONID=#{@jsession}; path=/; domain=localhost; HttpOnly;"
+  def parse_cookies
+    HTTP::Cookie.cookie_value(@cookies)
   end
 end
