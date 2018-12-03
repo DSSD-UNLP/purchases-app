2798816ce468b13dbcde75dfcb6865da4616814f
Julian Pasquale <julian.pasquale@snappler.com>
2018-12-03 00:47:06 -0300

    showing discounts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def finish_previous_case
-    if((request.referrer&.include? 'purchases/new') &&
-      ((params[:controller] != 'purchases') && (params[:action] != 'create')))
+    if request.referrer&.include? 'purchases/new'
+      return if (params[:controller] == 'purchases') && (params[:action] == 'create')
 
       last_case = Rails.cache.read("#{BONITA_HOST_URL}/last_case")
       BonitaManager.new(last_case).finish_case_with_error if last_case.present?
